9de59f751147448a217f3d0ba1cb5938bc10c90a
Adrian Smith <triode1@btinternet.com>
2012-12-07 22:45:47 +0000

    0.5beta2 - additional mp4 parsing capablity

--- squeezelite.h
+++ squeezelite.h
@@ -35,7 +35,7 @@
 #include <sys/types.h>
 #include <poll.h>
 
-#define VERSION "v0.5beta1"
+#define VERSION "v0.5beta2"
 
 #define STREAMBUF_SIZE (2 * 1024 * 1024)
 #define OUTPUTBUF_SIZE (44100 * 8 * 10)
