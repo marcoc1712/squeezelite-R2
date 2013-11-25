f71dbca6689e2d89a690a5d51f71a5fe6008dc6e
Adrian Smith <triode1@btinternet.com>
2013-11-25 21:50:19 +0000

    version 1.3.1 - support for compile time linking of all libraries

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE or RESAMPLE to influence build
 
-#define VERSION "v1.3"
+#define VERSION "v1.3.1"
 
 // build detection
 #if defined(linux)
@@ -76,7 +76,6 @@
 #undef FFMPEG
 #define FFMPEG    1
 #else
-#undef FFMPEG
 #define FFMPEG    0
 #endif
 
@@ -84,11 +83,20 @@
 #undef VISEXPORT
 #define VISEXPORT 1 // visulizer export support uses linux shared memory
 #else
-#undef VISEXPORT
 #define VISEXPORT 0
 #endif
 
-// dynamically loaded libraries
+#if defined(LINKALL)
+#undef LINKALL
+#define LINKALL   1 // link all libraries at build time - requires all to be available at run time
+#else
+#define LINKALL   0
+#endif
+
+
+#if !LINKALL
+
+// dynamically loaded libraries at run time
 #if LINUX
 #define LIBFLAC "libFLAC.so.8"
 #define LIBMAD  "libmad.so.0"
@@ -99,6 +107,7 @@
 #define LIBAVUTIL   "libavutil.so.%d"
 #define LIBAVCODEC  "libavcodec.so.%d"
 #define LIBAVFORMAT "libavformat.so.%d"
+#define LIBSOXR "libsoxr.so.0"
 #endif
 
 #if OSX
@@ -111,6 +120,7 @@
 #define LIBAVUTIL   "libavutil.%d.dylib"
 #define LIBAVCODEC  "libavcodec.%d.dylib"
 #define LIBAVFORMAT "libavformat.%d.dylib"
+#define LIBSOXR "libsoxr.0.dylib"
 #endif
 
 #if WIN
@@ -123,7 +133,10 @@
 #define LIBAVUTIL   "avutil-%d.dll"
 #define LIBAVCODEC  "avcodec-%d.dll"
 #define LIBAVFORMAT "avformat-%d.dll"
+#define LIBSOXR "libsoxr.dll"
 #endif
+
+#endif // !LINKALL
 
 // config options
 #define STREAMBUF_SIZE (2 * 1024 * 1024)
