1965bf43670d8aaf2c31ad7dd200fe4562644989
Adrian Smith <triode1@btinternet.com>
2014-01-29 21:46:18 +0000

    check for id32 tag at start of mp3, needed for localfile access case only fix typos

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.5"
+#define VERSION "v1.6-dev"
 
 // build detection
 #if defined(linux)
