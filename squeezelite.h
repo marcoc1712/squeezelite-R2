1ca20695176cb1d970d01ca47d5d27d85d540857
Adrian Smith <triode1@btinternet.com>
2014-03-23 20:22:13 +0000

    bump version to v1.6

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6-dev"
+#define VERSION "v1.6"
 
 // build detection
 #if defined(linux)
