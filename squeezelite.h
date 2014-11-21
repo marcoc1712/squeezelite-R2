9094ec549aa6162cb1edd70025faa4f701971540
Adrian Smith <triode1@btinternet.com>
2014-11-21 11:53:52 +0000

    trap use of hardware mac address, bump version

--- squeezelite.h
+++ squeezelite.h
@@ -20,7 +20,7 @@
 
 // make may define: PORTAUDIO, SELFPIPE, RESAMPLE, RESAMPLE_MP, VISEXPORT, DSD, LINKALL to influence build
 
-#define VERSION "v1.6.4"
+#define VERSION "v1.6.5"
 
 // build detection
 #if defined(linux)
