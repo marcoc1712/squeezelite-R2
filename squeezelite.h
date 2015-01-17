a3751843481b94657b763eb634957e954a322ab5
Adrian Smith <triode1@btinternet.com>
2015-01-17 18:56:59 +0000

    enforce alsa mixer min max, support non zero mixer index

--- squeezelite.h
+++ squeezelite.h
@@ -595,7 +595,7 @@ void list_mixers(const char *output_device);
 void set_volume(unsigned left, unsigned right);
 bool test_open(const char *device, unsigned rates[]);
 void output_init_alsa(log_level level, const char *device, unsigned output_buf_size, char *params, unsigned rates[], 
-					  unsigned rate_delay, unsigned rt_priority, unsigned idle, const char *volume_mixer);
+					  unsigned rate_delay, unsigned rt_priority, unsigned idle, char *volume_mixer);
 void output_close_alsa(void);
 #endif
 
