5021c16b559f937e28d3f4f1d333f743f45ae2b6
Adrian Smith <triode1@btinternet.com>
2015-01-18 18:51:12 +0000

    implement polarity inversion

--- squeezelite.h
+++ squeezelite.h
@@ -561,6 +561,7 @@ struct outputstate {
 	u8_t  *track_start;        // set in decode thread
 	u32_t gainL;               // set by slimproto
 	u32_t gainR;               // set by slimproto
+	int polarity;              // set by slimproto (1 or -1)
 	u32_t next_replay_gain;    // set by slimproto
 	unsigned threshold;        // set by slimproto
 	fade_state fade;
